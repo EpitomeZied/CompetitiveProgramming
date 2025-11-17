90370093e2a9a682e42a32135f45d9418289b0e5
Mahmoud Abo-Zied <116457362+mahmoudabozied4@users.noreply.github.com>
2025-11-17 21:13:23 +0200

    Create hackerlandRadioTransmitters.cpp

---
+++ HackerRank/hackerlandRadioTransmitters.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'hackerlandRadioTransmitters' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY x
+ *  2. INTEGER k
+ */
+
+int hackerlandRadioTransmitters(vector<int> v, int k) {
+    int n = v.size();
+    sort(v.begin() , v.end());
+    int i = 0 , ans = 0  ;
+    while ( i < n){
+        int l = i , r = n - 1;
+        int mx  = i ;
+        while ( l <= r ){
+            int m = l + (r - l) / 2;
+            if ( v[m] - v[i] <= k ){
+                l = m + 1;
+                mx = max(mx , m);
+            }else
+                r = m - 1;
+        }
+        i = mx ;
+        l = i , r = n - 1;
+        while ( l <= r ){
+            int m = l + (r - l) / 2;
+            if ( v[m] - v[i] <= k ){
+                l = m + 1;
+                mx = max(mx , m);
+            }else
+                r = m - 1;
+        }
+        i = mx + 1 ;
+        ans++;
+    }
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int k = stoi(first_multiple_input[1]);
+
+    string x_temp_temp;
+    getline(cin, x_temp_temp);
+
+    vector<string> x_temp = split(rtrim(x_temp_temp));
+
+    vector<int> x(n);
+
+    for (int i = 0; i < n; i++) {
+        int x_item = stoi(x_temp[i]);
+
+        x[i] = x_item;
+    }
+
+    int result = hackerlandRadioTransmitters(x, k);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
